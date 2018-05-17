75713d2dfc7abca1b0f2b37b138b480ff4961a3a
Josh Justice <jjustice@bignerdranch.com>
2018-05-17 06:01:51 -0400

    Get RN working

--- android/app/src/main/java/com/bignerdranch/android/reactnativeexistingandroid/MainActivity.java
+++ android/app/src/main/java/com/bignerdranch/android/reactnativeexistingandroid/MainActivity.java
@@ -1,13 +1,51 @@
 package com.bignerdranch.android.reactnativeexistingandroid;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int OVERLAY_PERMISSION_REQ_CODE = 1;
+
+    private Button mShowReactNativeThingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+        }
+
+        mShowReactNativeThingButton = (Button)findViewById(R.id.openReactNativeThingButton);
+        mShowReactNativeThingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MyReactActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // permission not granted
+                }
+            }
+        }
     }
 }
